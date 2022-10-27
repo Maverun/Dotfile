55e56112d3bdf61781e1ee1ee649a6ba3a02be60
Maverun <maverun@protonmail.ch>
2022-10-27 00:20:22 -0400

    Cleaning plugins as well... hackoberfest also

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -45,8 +45,6 @@ return require("packer").startup(
     use 'unblevable/quick-scope' -- Show highlight key for f,F,t,T, best thing.
     use 'simrat39/symbols-outline.nvim' --display tags
 
-    -- use 'kyazdani42/nvim-tree.lua'
-    -- use 'ggandor/lightspeed.nvim'
     use {'phaazon/hop.nvim',config=function() require'hop'.setup() end}
 
 -- ┌───────────────────────────────────────────────────────────────────────────┐
@@ -57,18 +55,12 @@ return require("packer").startup(
     use 'godlygeek/tabular'
     use 'plasticboy/vim-markdown' --vim markdown for vimwiki
     -- use 'gaoDean/autolist.nvim' --auto list for you.
-    -- use 'iamcco/markdown-preview.nvim'
-    -- use 'Pocco81/HighStr.nvim'
-    -- use 'alok/notational-fzf-vim'
-    -- use 'lervag/wiki.vim'
-    -- use 'jbyuki/venn.nvim'
 
 -- ┌───────────────────────────────────────────────────────────────────────────┐
 -- │                                LSP Relate                                 │
 -- └───────────────────────────────────────────────────────────────────────────┘
 
     use 'neovim/nvim-lspconfig'                             -- LSP Config that allow us to use instead of coc
-    -- use 'kabouzeid/nvim-lspinstall'                         -- Ease of Install Language Servers
     use 'williamboman/nvim-lsp-installer'
     use 'brymer-meneses/grammar-guard.nvim'
 
@@ -78,16 +70,13 @@ return require("packer").startup(
 
     use 'tpope/vim-fugitive'                                 -- GIT
     use 'tpope/vim-sleuth'                                 -- auto ajust shiftwidth/expandtab
-    -- use 'tpope/vim-commentary'
     use 'numtostr/FTerm.nvim'                               -- Floating Terminal
 
     --use 'junegunn/fzf'                                      -- Allowing Fuzzle Finder Search!
     --use 'junegunn/fzf.vim'                                  -- FZF well u know fuzzy finder thingy
 
     --use 'jalvesaq/Nvim-R'                                    -- In replace of Rstudio
-    -- use 'mizlan/iswap.nvim'                                  -- Allow to Swap params ease
     use 'ekickx/clipboard-image.nvim'                        -- Allow to paste img as a url of path (Auto create picture files locally)
-    -- use 'sudormrfbin/cheatsheet.nvim'                        -- Cheat sheet to remind you
     use 'Djancyp/cheat-sheet' -- using cheat.sh while in nvim.
     use {'nvim-treesitter/nvim-treesitter', run =':TSUpdate'}-- Treesitter rules all
     use 'nvim-treesitter/playground'                        -- Allow to Debug
@@ -113,15 +102,10 @@ return require("packer").startup(
     use "nvim-telescope/telescope-frecency.nvim"
 
     use { 'echasnovski/mini.nvim', branch = 'stable' }
-    -- use 'tjdevries/train.nvim'                               -- To be Gitgud, Rumor say master UP AND DOWN SON!
-    -- use 'ThePrimeagen/vim-be-good'                           -- be faster with moment. train train!
 
     use 'mfussenegger/nvim-dap'
     use 'theHamsta/nvim-dap-virtual-text'
     use 'rcarriga/nvim-dap-ui'
-    --use 'puremourning/vimspector'
-    -- use "bfredl/nvim-ipy"
-    -- use "ivanov/vim-ipython"
 
     use 'L3MON4D3/LuaSnip'
     use 'rafamadriz/friendly-snippets'
@@ -136,13 +120,11 @@ return require("packer").startup(
 --│                                  Orgmode                                   │
 --└────────────────────────────────────────────────────────────────────────────┘
 
-    use {'nvim-orgmode/orgmode', config = function()
-                require('orgmode').setup{}
-            end
-    }
+    -- use {'nvim-orgmode/orgmode', config = function()
+    --             require('orgmode').setup{}
+    --         end
+    -- }
 
-    -- use {'lukas-reineke/headlines.nvim',config = function() require'headlines'.setup() end}
-    -- use {'akinsho/org-bullets.nvim',config = function() require'org-bullets'.setup() end}
 
 
     use 'lervag/vimtex'
